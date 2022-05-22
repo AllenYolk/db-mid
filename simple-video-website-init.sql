4c61d1ae68330122e067dfa90002db7fb9a6b6bd
Allen-Yolk <2627099045@qq.com>
2022-05-22 21:59:51 +0800

    Fix bugs when generating primary and foreign keys.

--- simple-video-website-init.sql
+++ simple-video-website-init.sql
@@ -1,6 +1,6 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     2022/5/22 16:10:32                           */
+/* Created on:     2022/5/22 21:58:50                           */
 /*==============================================================*/
 
 
@@ -18,61 +18,73 @@ drop view if exists view_section_video_count;
 
 drop view if exists view_video_data;
 
-drop index follower_FK on follow_record;
+drop index follower_idx on follow_record;
 
-drop index followed_FK on follow_record;
+drop index followed_idx on follow_record;
 
 drop table if exists follow_record;
 
-drop index related_FK on related_video;
+drop index related_idx on related_video;
 
-drop index main_FK on related_video;
+drop index main_idx on related_video;
 
 drop table if exists related_video;
 
-drop index section_PK on section;
+drop index section_idx on section;
 
 drop table if exists section;
 
-drop index user_add2fav_video2_FK on user_add2fav_video;
+drop index fav_video_idx on user_add2fav_video;
 
-drop index user_add2fav_video_FK on user_add2fav_video;
+drop index fav_user_idx on user_add2fav_video;
 
 drop table if exists user_add2fav_video;
 
-drop index user_like_video2_FK on user_like_video;
+drop index like_video_idx on user_like_video;
 
-drop index user_like_video_FK on user_like_video;
+drop index like_user_idx on user_like_video;
 
 drop table if exists user_like_video;
 
-drop index user_publish_video2_FK on user_publish_video;
+drop index publish_video_idx on user_publish_video;
 
-drop index user_publish_video_FK on user_publish_video;
+drop index publish_user_idx on user_publish_video;
 
 drop table if exists user_publish_video;
 
-drop index user_watch_video2_FK on user_watch_video;
+drop index watch_video_idx on user_watch_video;
 
-drop index user_watch_video_FK on user_watch_video;
+drop index watch_user_idx on user_watch_video;
 
 drop table if exists user_watch_video;
 
-drop index video_in_section_FK on video;
+drop index video_section_idx on video;
 
-drop index video_PK on video;
+drop index video_idx on video;
 
 drop table if exists video;
 
-drop index user_make_comment_FK on video_comment;
+drop index comment_user_idx on video_comment;
 
-drop index comment_below_video_FK on video_comment;
+drop index comment_video_idx on video_comment;
 
 drop table if exists video_comment;
 
-drop index user_PK on website_user;
+drop index user_idx on website_user;
 
 drop table if exists website_user;
+
+/*==============================================================*/
+/* Table: website_user                                          */
+/*==============================================================*/
+create table website_user
+(
+   user_id              bigint not null,
+   user_name            varchar(25) not null,
+   user_exp             int not null,
+   user_vip             bool not null,
+   primary key (user_id)
+);
 
 /*==============================================================*/
 /* Table: follow_record                                         */
@@ -81,48 +93,28 @@ create table follow_record
 (
    followed_id          bigint not null,
    follower_id          bigint not null,
-   follow_time          timestamp not null
+   follow_time          timestamp not null,
+   primary key (followed_id, follower_id),
+   constraint FK_followed foreign key (followed_id)
+      references website_user (user_id) on delete restrict on update restrict,
+   constraint FK_follower foreign key (follower_id)
+      references website_user (user_id) on delete restrict on update restrict
 );
 
 /*==============================================================*/
-/* Index: followed_FK                                           */
+/* Index: followed_idx                                          */
 /*==============================================================*/
-create index followed_FK on follow_record
+create index followed_idx on follow_record
 (
    followed_id
 );
 
 /*==============================================================*/
-/* Index: follower_FK                                           */
+/* Index: follower_idx                                          */
 /*==============================================================*/
-create index follower_FK on follow_record
+create index follower_idx on follow_record
 (
    follower_id
-);
-
-/*==============================================================*/
-/* Table: related_video                                         */
-/*==============================================================*/
-create table related_video
-(
-   main_video_id        varchar(20) not null,
-   related_video_id     varchar(20) not null
-);
-
-/*==============================================================*/
-/* Index: main_FK                                               */
-/*==============================================================*/
-create index main_FK on related_video
-(
-   main_video_id
-);
-
-/*==============================================================*/
-/* Index: related_FK                                            */
-/*==============================================================*/
-create index related_FK on related_video
-(
-   related_video_id
 );
 
 /*==============================================================*/
@@ -131,116 +123,10 @@ create index related_FK on related_video
 create table section
 (
    section_name         varchar(20) not null,
-   administrator_id     bigint not null
-);
-
-/*==============================================================*/
-/* Index: section_PK                                            */
-/*==============================================================*/
-create unique index section_PK on section
-(
-   section_name
-);
-
-/*==============================================================*/
-/* Table: user_add2fav_video                                    */
-/*==============================================================*/
-create table user_add2fav_video
-(
-   user_id              bigint not null,
-   video_id             varchar(20) not null
-);
-
-/*==============================================================*/
-/* Index: user_add2fav_video_FK                                 */
-/*==============================================================*/
-create index user_add2fav_video_FK on user_add2fav_video
-(
-   user_id
-);
-
-/*==============================================================*/
-/* Index: user_add2fav_video2_FK                                */
-/*==============================================================*/
-create index user_add2fav_video2_FK on user_add2fav_video
-(
-   video_id
-);
-
-/*==============================================================*/
-/* Table: user_like_video                                       */
-/*==============================================================*/
-create table user_like_video
-(
-   user_id              bigint not null,
-   video_id             varchar(20) not null
-);
-
-/*==============================================================*/
-/* Index: user_like_video_FK                                    */
-/*==============================================================*/
-create index user_like_video_FK on user_like_video
-(
-   user_id
-);
-
-/*==============================================================*/
-/* Index: user_like_video2_FK                                   */
-/*==============================================================*/
-create index user_like_video2_FK on user_like_video
-(
-   video_id
-);
-
-/*==============================================================*/
-/* Table: user_publish_video                                    */
-/*==============================================================*/
-create table user_publish_video
-(
-   user_id              bigint not null,
-   video_id             varchar(20) not null
-);
-
-/*==============================================================*/
-/* Index: user_publish_video_FK                                 */
-/*==============================================================*/
-create index user_publish_video_FK on user_publish_video
-(
-   user_id
-);
-
-/*==============================================================*/
-/* Index: user_publish_video2_FK                                */
-/*==============================================================*/
-create index user_publish_video2_FK on user_publish_video
-(
-   video_id
-);
-
-/*==============================================================*/
-/* Table: user_watch_video                                      */
-/*==============================================================*/
-create table user_watch_video
-(
-   video_id             varchar(20) not null,
-   user_id              bigint not null,
-   watch_time           timestamp not null
-);
-
-/*==============================================================*/
-/* Index: user_watch_video_FK                                   */
-/*==============================================================*/
-create index user_watch_video_FK on user_watch_video
-(
-   video_id
-);
-
-/*==============================================================*/
-/* Index: user_watch_video2_FK                                  */
-/*==============================================================*/
-create index user_watch_video2_FK on user_watch_video
-(
-   user_id
+   administrator_id     bigint not null,
+   primary key (section_name),
+   constraint FK_administer foreign key (administrator_id)
+      references website_user (user_id) on delete restrict on update restrict
 );
 
 /*==============================================================*/
@@ -254,21 +140,183 @@ create table video
    video_length         int not null,
    video_publish_time   timestamp not null,
    video_require_vip    bool not null,
-   video_iframe_url     varchar(256)
+   video_iframe_url     varchar(256),
+   primary key (video_id),
+   constraint FK_video_in_section foreign key (section_name)
+      references section (section_name) on delete restrict on update restrict
 );
 
 /*==============================================================*/
-/* Index: video_PK                                              */
+/* Table: related_video                                         */
 /*==============================================================*/
-create unique index video_PK on video
+create table related_video
+(
+   main_video_id        varchar(20) not null,
+   related_video_id     varchar(20) not null,
+   primary key (main_video_id, related_video_id),
+   constraint FK_main foreign key (main_video_id)
+      references video (video_id) on delete restrict on update restrict,
+   constraint FK_related foreign key (related_video_id)
+      references video (video_id) on delete restrict on update restrict
+);
+
+/*==============================================================*/
+/* Index: main_idx                                              */
+/*==============================================================*/
+create index main_idx on related_video
+(
+   main_video_id
+);
+
+/*==============================================================*/
+/* Index: related_idx                                           */
+/*==============================================================*/
+create index related_idx on related_video
+(
+   related_video_id
+);
+
+/*==============================================================*/
+/* Index: section_idx                                           */
+/*==============================================================*/
+create index section_idx on section
+(
+   section_name
+);
+
+/*==============================================================*/
+/* Table: user_add2fav_video                                    */
+/*==============================================================*/
+create table user_add2fav_video
+(
+   user_id              bigint not null,
+   video_id             varchar(20) not null,
+   primary key (user_id, video_id),
+   constraint FK_user_add2fav_video foreign key (user_id)
+      references website_user (user_id) on delete restrict on update restrict,
+   constraint FK_user_add2fav_video2 foreign key (video_id)
+      references video (video_id) on delete restrict on update restrict
+);
+
+/*==============================================================*/
+/* Index: fav_user_idx                                          */
+/*==============================================================*/
+create index fav_user_idx on user_add2fav_video
+(
+   user_id
+);
+
+/*==============================================================*/
+/* Index: fav_video_idx                                         */
+/*==============================================================*/
+create index fav_video_idx on user_add2fav_video
 (
    video_id
 );
 
 /*==============================================================*/
-/* Index: video_in_section_FK                                   */
+/* Table: user_like_video                                       */
 /*==============================================================*/
-create index video_in_section_FK on video
+create table user_like_video
+(
+   user_id              bigint not null,
+   video_id             varchar(20) not null,
+   primary key (user_id, video_id),
+   constraint FK_user_like_video foreign key (user_id)
+      references website_user (user_id) on delete restrict on update restrict,
+   constraint FK_user_like_video2 foreign key (video_id)
+      references video (video_id) on delete restrict on update restrict
+);
+
+/*==============================================================*/
+/* Index: like_user_idx                                         */
+/*==============================================================*/
+create index like_user_idx on user_like_video
+(
+   user_id
+);
+
+/*==============================================================*/
+/* Index: like_video_idx                                        */
+/*==============================================================*/
+create index like_video_idx on user_like_video
+(
+   video_id
+);
+
+/*==============================================================*/
+/* Table: user_publish_video                                    */
+/*==============================================================*/
+create table user_publish_video
+(
+   user_id              bigint not null,
+   video_id             varchar(20) not null,
+   primary key (user_id, video_id),
+   constraint FK_user_publish_video foreign key (user_id)
+      references website_user (user_id) on delete restrict on update restrict,
+   constraint FK_user_publish_video2 foreign key (video_id)
+      references video (video_id) on delete restrict on update restrict
+);
+
+/*==============================================================*/
+/* Index: publish_user_idx                                      */
+/*==============================================================*/
+create index publish_user_idx on user_publish_video
+(
+   user_id
+);
+
+/*==============================================================*/
+/* Index: publish_video_idx                                     */
+/*==============================================================*/
+create index publish_video_idx on user_publish_video
+(
+   video_id
+);
+
+/*==============================================================*/
+/* Table: user_watch_video                                      */
+/*==============================================================*/
+create table user_watch_video
+(
+   video_id             varchar(20) not null,
+   user_id              bigint not null,
+   watch_time           timestamp not null,
+   primary key (video_id, user_id),
+   constraint FK_user_watch_video foreign key (video_id)
+      references video (video_id) on delete restrict on update restrict,
+   constraint FK_user_watch_video2 foreign key (user_id)
+      references website_user (user_id) on delete restrict on update restrict
+);
+
+/*==============================================================*/
+/* Index: watch_user_idx                                        */
+/*==============================================================*/
+create index watch_user_idx on user_watch_video
+(
+   user_id
+);
+
+/*==============================================================*/
+/* Index: watch_video_idx                                       */
+/*==============================================================*/
+create index watch_video_idx on user_watch_video
+(
+   video_id
+);
+
+/*==============================================================*/
+/* Index: video_idx                                             */
+/*==============================================================*/
+create unique index video_idx on video
+(
+   video_id
+);
+
+/*==============================================================*/
+/* Index: video_section_idx                                     */
+/*==============================================================*/
+create index video_section_idx on video
 (
    section_name
 );
@@ -282,40 +330,34 @@ create table video_comment
    comment_seqnum       int not null,
    user_id              bigint not null,
    comment_time         timestamp not null,
-   comment_text         varchar(256) not null
+   comment_text         varchar(256) not null,
+   primary key (video_id, comment_seqnum),
+   constraint FK_comment_below_video foreign key (video_id)
+      references video (video_id) on delete restrict on update restrict,
+   constraint FK_user_make_comment foreign key (user_id)
+      references website_user (user_id) on delete restrict on update restrict
 );
 
 /*==============================================================*/
-/* Index: comment_below_video_FK                                */
+/* Index: comment_video_idx                                     */
 /*==============================================================*/
-create index comment_below_video_FK on video_comment
+create index comment_video_idx on video_comment
 (
    video_id
 );
 
 /*==============================================================*/
-/* Index: user_make_comment_FK                                  */
+/* Index: comment_user_idx                                      */
 /*==============================================================*/
-create index user_make_comment_FK on video_comment
+create index comment_user_idx on video_comment
 (
    user_id
 );
 
 /*==============================================================*/
-/* Table: website_user                                          */
+/* Index: user_idx                                              */
 /*==============================================================*/
-create table website_user
-(
-   user_id              bigint not null,
-   user_name            varchar(25) not null,
-   user_exp             int not null,
-   user_vip             bool not null
-);
-
-/*==============================================================*/
-/* Index: user_PK                                               */
-/*==============================================================*/
-create unique index user_PK on website_user
+create unique index user_idx on website_user
 (
    user_id
 );
